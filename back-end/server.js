7506d34b3ba34a3512b9f5bd51a529f1262681a1
zq <1078538559@qq.com>
2018-12-21 10:34:53 +0800

    增加地址页数据接口

--- back-end/server.js
+++ back-end/server.js
@@ -4,12 +4,14 @@ var app = express();
 // 引入路由模块
 var FilmRouter = require('./routes/film.js');
 var UserRouter = require('./routes/user.js');
-var cinemaRouter = require('./routes/cinema.js');
+var CinemaRouter = require('./routes/cinema.js');
+var AddressRouter = require('./routes/address.js');
 
 // 使用路由模块，中间件
 app.use('/api/film/', FilmRouter);
 app.use('/api/user/', UserRouter);
-app.use('/api/cinema/', cinemaRouter);
+app.use('/api/cinema/', CinemaRouter);
+app.use('/api/address/',AddressRouter);
 
 app.listen(3000);
 console.log('服务启动成功');
